8a13fa002f8fba9de77087ce2cea991857689b3c
Aldi <aldiponorogo70@gmail.com>
2021-07-16 04:29:30 +0700

    selesai

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ header={
 }
 
 url = 'https://store.steampowered.com/explore/new/'
-
 r = requests.get(url, header)
 soup = bs4.BeautifulSoup(r.text,"html.parser")
+rilis_baru = soup.find('div',{'id':'tab_newreleases_content'})
+rilis_barus = soup.findAll('a', {'class':'tab_item'})
+for barusan in rilis_barus:
+    image = barusan.find('img',{'class':'tab_item_cap_img'})['src']
+    title = barusan.find('div',{'class':'tab_item_name'}).text
+    respon = requests.get(image,header,stream=True)
+    filename= image.split("/")[-1].split("?")[0]
+    ext = filename[-4:]
+    if respon.ok:
+        with open('images'+ re.sub(r'(?u)[^-\w.]','_',title) + ext,'wb') as a:
+            a.write(respon.content)
+
+
+
